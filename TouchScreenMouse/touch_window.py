0f21e2758460113185f505a06679d264ed4cfd59
VediYD <vedi.yash@gmail.com>
2019-12-29 06:52:01 +0530

    now using rpyc to connect to a local server which would help controlling the mouse

--- TouchScreenMouse/touch_window.py
+++ TouchScreenMouse/touch_window.py
@@ -1,20 +1,25 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from rpyc import connect
 
 
 # class to define the widget
 class TouchInput(Widget):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.server_connection = connect('localhost', 7777)
 
     def on_touch_down(self, touch):
-        print('DOWN>>', touch.sx, touch.sy, touch.sz)
+        print('DOWN>>', touch.sx, touch.sy)
+        self.server_connection.root.move_cursor(touch.sx, touch.sy)
 
     def on_touch_up(self, touch):
-        print('UP>>', touch.sx, touch.sy, touch.sz)
+        print('UP>>', touch.sx, touch.sy)
+        self.server_connection.root.move_cursor(touch.sx, touch.sy)
 
     def on_touch_move(self, touch):
-        print('MOVING>>', touch.sx, touch.sy, touch.sz)
+        print('MOVING>>', touch.sx, touch.sy)
+        self.server_connection.root.move_cursor(touch.sx, touch.sy)
 
 
 # class to build the app
